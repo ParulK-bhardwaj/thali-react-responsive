a22d6529dbc7883151d6b5caab91a8667fbde4f9
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-05-10 13:23:57 -0400

    README

--- src/components/FoodDetails/FoodDetails.js
+++ src/components/FoodDetails/FoodDetails.js
@@ -30,9 +30,9 @@ function FoodDetails(props) {
           {city}, {state}
         </p>
       </div>
-      <div className='FoodDetails-image w-full md:w-1/2 h-100 py-5 px-2 rounded-lg overflow-hidden order-1 md:order-2'>
+      <div className='FoodDetails-image w-full md:w-1/2  py-5 px-2 overflow-hidden order-1 md:order-2'>
         <img src={`${process.env.PUBLIC_URL}/images/${images}`} 
-        alt={name} className="h-full" />
+        alt={name} className="h-4/5 w-full flex-1 rounded-lg border" />
       </div>
     </section>
   )
